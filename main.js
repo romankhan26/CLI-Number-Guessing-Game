ce0b87439ee4bb9ae9c6dbedd1c91854d6680952
roman khan <romankhan.26@outlook.com>
2024-04-07 01:59:50 +0500

    version update

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
 #! /usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var inquirer_1 = require("inquirer");
-var chalk_1 = require("chalk");
-var randomNumber = Math.floor(Math.random() * 4 + 1);
-var answers = await inquirer_1.default.prompt([
+import inquirer from "inquirer";
+import chalk from "chalk";
+const randomNumber = Math.floor(Math.random() * 4 + 1);
+const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
-        message: chalk_1.default.bold.blue("Please guess a number in between 1-4")
+        message: chalk.bold.blue("Please guess a number in between 1-4")
     }
 ]);
 if (answers === randomNumber) {
-    console.log(chalk_1.default.green.bold.italic("congratulations! You have guessed a right number!"));
+    console.log(chalk.green.bold.italic("congratulations! You have guessed a right number!"));
 }
 else {
-    console.log(chalk_1.default.red.bold.italic("Sorry! You've guessed a wrong number!"));
+    console.log(chalk.red.bold.italic("Sorry! You've guessed a wrong number!"));
 }
